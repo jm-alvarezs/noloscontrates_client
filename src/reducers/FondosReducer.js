28e3d86fed705072afde3f07d9f79c0c92b4ebc0
Juan Manuel Alvarez <43937673+jm-alvarezs@users.noreply.github.com>
2020-05-26 23:20:47 -0500

    added if for fondos

--- src/reducers/FondosReducer.js
+++ src/reducers/FondosReducer.js
@@ -13,6 +13,7 @@ export default (state = initialState, { type, payload }) => {
         const fondo = payload[0].urls.regular;
         return { ...state, fondos: payload, fondo, current: 1, page: state.page + 1 };
     case SET_FONDO:        
+        if(state.fondos === null) return { ...state };
         const background = state.fondos[state.current].urls.regular;
         return { ...state, fondo: background };
     default:
